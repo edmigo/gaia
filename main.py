feaa82cd58d5c8ae7158504ed29f9fbfec310d63
Oleg Khaskin <okhaskin@gmail.com>
2024-01-23 17:23:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -384,8 +384,8 @@ with st.sidebar:
         st.session_state["uploaded_files"] = []
 
     image_url = 'GAIA_black.png'
-    image = Image.open(image_url)
-    st.image(image)
+    #image = Image.open(image_url)
+    #st.image(image)
 
     original_title = '<p style="font-family:prometo trial; color:white; font-size: 20px; font-weight: bold; ">Model Settings</p>'
     st.markdown(original_title, unsafe_allow_html=True)
